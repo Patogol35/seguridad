0f2052842f7be9aed222b9ed2e3c19b594667052
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-09 11:54:43 -0500

    Update headers.py

--- app/scanners/headers.py
+++ app/scanners/headers.py
@@ -1,13 +1,16 @@
 import requests
+
 SECURITY_HEADERS = [
     "Content-Security-Policy",
     "X-Frame-Options",
     "Strict-Transport-Security",
     "X-Content-Type-Options"
 ]
+
 def scan_headers(url):
-    response = requests.get(url)
-    missing = [h for h in SECURITY_HEADERS if h not in response.headers]
-    return {
-        "missing_headers": missing
-    }
+    try:
+        response = requests.get(url, timeout=5)
+        missing = [h for h in SECURITY_HEADERS if h not in response.headers]
+        return {"missing_headers": missing}
+    except requests.RequestException:
+        return {"missing_headers": SECURITY_HEADERS}
