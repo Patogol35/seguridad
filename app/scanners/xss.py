7b8612d5f2c83370e817bac3b502845c8ae937b6
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-09 11:59:50 -0500

    Update xss.py

--- app/scanners/xss.py
+++ app/scanners/xss.py
@@ -1,14 +1,17 @@
-import requests
+import httpx
+from app.core.config import DEFAULT_TIMEOUT, HEADERS
 
 XSS_PAYLOAD = "<script>alert(1)</script>"
 
-def scan_xss(url):
+async def scan_xss(url: str):
     try:
-        response = requests.get(url, params={"q": XSS_PAYLOAD}, timeout=5)
-        vulnerable = XSS_PAYLOAD in response.text
-        return {
-            "vulnerable": vulnerable,
-            "payload": XSS_PAYLOAD
-        }
-    except requests.RequestException:
+        async with httpx.AsyncClient(timeout=DEFAULT_TIMEOUT, headers=HEADERS) as client:
+            response = await client.get(url, params={"q": XSS_PAYLOAD})
+            vulnerable = XSS_PAYLOAD in response.text
+
+            return {
+                "vulnerable": vulnerable,
+                "payload": XSS_PAYLOAD
+            }
+    except httpx.RequestError:
         return {"vulnerable": False}
