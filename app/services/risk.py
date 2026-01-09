6d99720f9b673f49bf1313076cc12a7a0e8f3420
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-09 11:54:21 -0500

    Update risk.py

--- app/services/risk.py
+++ app/services/risk.py
@@ -1,14 +1,21 @@
 def calculate_risk(xss, sqli, headers):
     score = 0
-    if sqli["vulnerable"]:
+
+    if sqli.get("vulnerable"):
         score += 50
-    if xss["vulnerable"]:
+    if xss.get("vulnerable"):
         score += 30
-    score += len(headers["missing_headers"]) * 5
+
+    score += len(headers.get("missing_headers", [])) * 5
+
     if score >= 70:
         level = "ALTO"
     elif score >= 40:
         level = "MEDIO"
     else:
         level = "BAJO"
-    return {"score": score, "level": level}
+
+    return {
+        "score": score,
+        "level": level
+    }
