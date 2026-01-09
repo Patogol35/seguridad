c88a0b774e773b06c3dd8795ca8f8cd4d1f556ab
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-09 11:53:08 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,12 +5,16 @@ from app.scanners.sqli import scan_sqli
 from app.scanners.headers import scan_headers
 from app.services.risk import calculate_risk
 from app.services.report import generate_report
+
 app = FastAPI(title="Web Security Analyzer")
+
 @app.post("/scan")
 def scan_website(data: ScanRequest):
     xss = scan_xss(data.url)
     sqli = scan_sqli(data.url)
     headers = scan_headers(data.url)
+
     risk = calculate_risk(xss, sqli, headers)
     report = generate_report(data.url, xss, sqli, headers, risk)
+
     return report
