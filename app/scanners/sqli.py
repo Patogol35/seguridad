ddf77a91e764ef7c41219dae4b55eb03f51073b7
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-01-09 14:34:35 -0500

    Update sqli.py

--- app/scanners/sqli.py
+++ app/scanners/sqli.py
@@ -1,16 +1,30 @@
 import httpx
+from app.core.config import DEFAULT_TIMEOUT, HEADERS
 
 SQL_PAYLOAD = "' OR '1'='1"
 
+
 async def scan_sqli(url: str):
     try:
-        async with httpx.AsyncClient(timeout=8.0) as client:
-            response = await client.get(url, params={"id": SQL_PAYLOAD})
-            errors = ["sql", "syntax", "mysql", "postgres"]
-            vulnerable = any(e in response.text.lower() for e in errors)
-            return {
-                "vulnerable": vulnerable,
-                "payload": SQL_PAYLOAD
-            }
+        async with httpx.AsyncClient(
+            timeout=DEFAULT_TIMEOUT,
+            headers=HEADERS
+        ) as client:
+            response = await client.get(
+                url,
+                params={"id": SQL_PAYLOAD}
+            )
+
+        errors = ["sql", "syntax", "mysql", "postgres"]
+        vulnerable = any(
+            e in response.text.lower()
+            for e in errors
+        )
+
+        return {
+            "vulnerable": vulnerable,
+            "payload": SQL_PAYLOAD
+        }
+
     except httpx.RequestError:
         return {"vulnerable": False}
